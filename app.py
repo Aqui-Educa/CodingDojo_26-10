cee711fb4679d784c7c206f303ec39653df64eb4
Marlon Almino <alminomarlon@gmail.com>
2021-10-26 20:12:40 -0300

    starting project

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/cadastro', methods=['GET'])
+def obterCadastro():
+    return render_template('cadastro.html')
