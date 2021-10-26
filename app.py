0bd4bc3b819902c207d3276bf3cebfb38d7e68f3
Victor <victorfalmeida12@gmail.com>
2021-10-26 20:37:35 -0300

    html + javascript

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/cadastro', methods=['GET'])
+@app.route('/', methods=['GET'])
 def obterCadastro():
-    return render_template('cadastro.html')
+    return render_template('template.html')
+
+
