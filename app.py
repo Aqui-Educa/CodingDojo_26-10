35746d6682e66f9934a17c1ddf30856dcbf46213
Victor <victorfalmeida12@gmail.com>
2021-10-26 23:42:05 -0300

    erros consertados com comentarios

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import csv
+import csv, json
 
 app = Flask(__name__)
 
@@ -11,18 +11,13 @@ def obterCadastro():
 
 @app.route('/cadastrarUsuario', methods=['POST'])
 def cadastrarUsuario():
-
-    usuario = request.json
-    with open("cadastro.csv", 'a', encoding="UTF-8", newline="") as cad:
-
-        write = csv.writer(cad, delimiter=';')
-
-        print(usuario)
-        user = [usuario['id'], usuario['nome'], usuario['cpf'], usuario['telefone'],
-                usuario['email'], usuario['vaga'], usuario['endereco']]
-        print(usuario)
-        print(user)
-
+    # antes era request.json 
+    usuario = request.data
+    # usuario retorna uma string, abaixo converte em json
+    usuario = json.loads(usuario)
+    with open("cadastro.csv", 'a', newline="") as cad:
+        write = csv.writer(cad, delimiter=',')
+        user = [usuario['id'], usuario['nome'], usuario['cpf'], usuario['telefone'], usuario['email'], usuario['vaga'], usuario['endereco']]
         write.writerow(user)
 
-    return "Cadastro realizado com sucesso!"
+    return {'message': 'Salvo com sucesso (CONSOLE)'}
