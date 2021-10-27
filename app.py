cb60c63199257ba297396ea022d05ba7dbab0b95
Rafael Aquino <rjoseaquino@gmail.com>
2021-10-26 21:01:22 -0300

    commint Rafael

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -8,3 +9,12 @@ def obterCadastro():
     return render_template('template.html')
 
 
+@app.route('/cadastrarUsuario', methods=['POST'])
+def cadastrarUsuario():
+    with open ("cadastro.csv", 'w', encoding="UTF-8", newline="") as cad:
+        write = csv.writer(cad, delimiter=',')
+        usuario = request.json
+        
+
+
+        return usuario
